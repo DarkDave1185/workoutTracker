c7257051e8d54a727db0614df3cb1052fdaeca30
DarkDave <darkdave1185@gmail.com>
2020-10-15 18:12:47 -0700

    finish and tested workoutTracker in Compass

---
+++ model/workout.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: new Date(),
+  },
+
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Please Enter Excersise name",
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Please enter name",
+      },
+      duration: {
+        type: Number,
+      },
+      weight: {
+        type: Number,
+        required: "Enter Weight",
+      },
+      reps: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      distance: {
+        type: Number,
+      },
+    },
+  ],
+});
+
+const Workout = mongoose.model("workout", workoutSchema);
+module.exports = Workout;
---
+++ routes/apiRoute.js
@@ -0,0 +1,60 @@
+const { response } = require("express");
+const db = require("../model");
+
+module.exports = function (app) {
+  app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+      .sort({ day: -1 })
+      .limit(1)
+      .then(function (data) {
+        console.log("testing the api call");
+        res.json(data);
+      });
+  });
+
+  app.post("/api/workouts", (req, res) => {
+    // getting data from front end
+
+    // POST is only creating the ID and PUT will fill the document created by POST
+    db.Workout.create({
+      day: new Date().setDate(new Date().getDate()),
+    }).then((response) => {
+      res.json(response);
+      console.log(response);
+    });
+  });
+
+  app.put("/api/workouts/:id", (req, res) => {
+    let params = req.params;
+
+    let input = req.body;
+    console.log(input);
+
+    db.Workout.updateOne(
+      { _id: params.id },
+      {
+        $push: {
+          exercises: [
+            {
+              type: input.type,
+              name: input.name,
+              duration: input.duration,
+              distance: input.distance,
+              weight: input.weight,
+              reps: input.reps,
+              sets: input.sets,
+            },
+          ],
+        },
+      }
+    ).then((response) => {
+      console.log(response);
+      res.json(response);
+    });
+  });
+
+  app.get("/api/workouts/range", (req, res) => {
+    // finding all exercises
+    db.Workout.find({}).then((response) => res.json(response));
+  });
+};
